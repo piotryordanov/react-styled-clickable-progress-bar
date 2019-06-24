14908915ff352ede414fdd4872cae79b08c9f8c6
Alpha <versari@pm.me>
2019-06-24 18:55:09 +0200

    Trying with something simple

--- src/index.js
+++ src/index.js
@@ -1,94 +1,12 @@
-/* eslint no-undef: 0 */
-// import PropTypes from 'prop-types'
-// Import styled from 'styled-components'
-// import React, {useState, useEffect} from 'react'
 import React from 'react'
 
-// Const Bar = styled.div`
-// 	margin: 0;
-// 	padding: 0;
-// 	position: fixed;
-// 	z-index: 99;
-// 	top: ${props => props.top};
-// 	left: ${props => props.left};
-// 	height: ${props => props.height};
-// 	width: ${props => props.width};
-// 	background-color: ${props => props.color};
-// 	:hover {
-// 		cursor: pointer;
-// 	}
-// `
-//
-// const onClick = e => {
-// 	const curr = e.pageX
-// 	const width = e.currentTarget.clientWidth
-// 	const height = window.document.body.offsetHeight - window.innerHeight
-// 	const ratio = (curr * 100) / width
-// 	window.scrollTo(0, (height * ratio) / 100)
-// }
-
-const ProgressBar = () => {
-	// Const [width, updateWidth] = useState(null)
-	console.log('here')
-	try {
-		console.log(document)
-	} catch {
-		console.log('document aint defined')
-	}
-
-	try {
-		console.log(window)
-	} catch {
-		console.log('window aint defined')
-	}
-
-	console.log(window)
-	return <div>Hello world</div>
-	// UseEffect(() => {
-	// 	const Scrolling = () => {
-	// 		const winScroll =
-	// 			document.body.scrollTop || document.documentElement.scrollTop
-	// 		const height =
-	// 			document.documentElement.scrollHeight -
-	// 			document.documentElement.clientHeight
-	// 		const scrolled = (winScroll / height) * 100
-	// 		if (height > 0) {
-	// 			updateWidth(`${scrolled}%`)
-	// 		} else {
-	// 			updateWidth(null)
-	// 		}
-	// 	}
-	//
-	// 	window.addEventListener('scroll', Scrolling)
-	// 	return () => window.removeEventListener('scroll', Scrolling)
-	// }, [])
-	//
-	// return typeof window === 'object' && typeof document === 'object' ? (
-	// 	<>
-	// 		<Bar width={width} {...props} />
-	// 		<Bar
-	// 			width="100%"
-	// 			onClick={e => onClick(e, updateWidth)}
-	// 			{...props}
-	// 			color="transparent"
-	// 		/>
-	// 	</>
-	// ) : (
-	// 	<> </>
-	// )
+const ProgressBar = props => {
+  return <div> Hello world </div>
 }
 
-// ProgressBar.propTypes = {
-// 	top: PropTypes.number,
-// 	left: PropTypes.number,
-// 	height: PropTypes.number,
-// 	color: PropTypes.string
-// }
-// ProgressBar.defaultProps = {
-// 	top: '0px',
-// 	left: '0px',
-// 	height: '5px',
-// 	color: 'black'
-// }
+ProgressBar.propTypes = {
+}
+ProgressBar.defaultProps = {
+}
 
 export default ProgressBar
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,20 @@
-const path = require('path')
-const pkg = require('./package.json')
-
-const libraryName = pkg.name
+var path = require('path');
 
 module.exports = {
-	entry: path.join(__dirname, 'src', 'index.js'),
-	output: {
-		path: path.join(__dirname, 'build'),
-		filename: 'index.js',
-		library: libraryName,
-		libraryTarget: 'commonjs2'
-	},
-	module: {
-		rules: [
-			{
-				test: /.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
-				}
-			}
-		]
-	},
-	externals: {
-		// Don't bundle react or react-dom
-		react: {
-			commonjs: 'react',
-			commonjs2: 'react',
-			amd: 'React',
-			root: 'React'
-		},
-		'react-dom': {
-			commonjs: 'react-dom',
-			commonjs2: 'react-dom',
-			amd: 'ReactDOM',
-			root: 'ReactDOM'
-		}
-	}
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('build'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader'
+            }
+        ]
+    }
 }
